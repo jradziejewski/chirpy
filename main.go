9d9f8b98d42b1d23aca17edc2067c225416d1de2
jradziejewski <192462@student.ue.wroc.pl>
2025-01-17 16:35:30 +0100

    use pointer in http server

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	servemux := http.NewServeMux()
 	servemux.Handle("/", http.FileServer(http.Dir(".")))
-	server := http.Server{
+	server := &http.Server{
 		Handler: servemux,
 		Addr:    ":8080",
 	}
