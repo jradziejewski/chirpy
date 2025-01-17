57e11dbd17152d6c39ddb43cb38682bbf9a7b438
jradziejewski <192462@student.ue.wroc.pl>
2025-01-17 16:32:08 +0100

    add simple fileserver

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func main() {
 	servemux := http.NewServeMux()
+	servemux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Handler: servemux,
 		Addr:    ":8080",
