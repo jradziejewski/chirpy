2cf7ebbacfa6bc04aeaca60394b65bd4a1138bc5
jradziejewski <192462@student.ue.wroc.pl>
2025-01-22 14:24:55 +0100

    add make jwt function in auth pkg + test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,6 +2,10 @@ package auth
 
 import (
 	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func TestHashPassword(t *testing.T) {
@@ -30,5 +34,39 @@ func TestCheckPasswordHash(t *testing.T) {
 	err = CheckPasswordHash("random", hashedPswrd)
 	if err == nil {
 		t.Fatalf(`CheckPasswordHash(%s, %s): expected error, got no error`, pswrd, hashedPswrd)
+	}
+}
+
+func TestMakeJWT(t *testing.T) {
+	userID := uuid.New()
+	tokenSecret := "supersecret"
+	expiresAt := time.Minute * 5
+
+	tokenString, err := MakeJWT(userID, tokenSecret, expiresAt)
+	if err != nil {
+		t.Fatalf("MakeJWT(%v, %v, %v): expected no error, got %v", userID, tokenSecret, expiresAt, err)
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil })
+	if err != nil {
+		t.Fatalf("MakeJWT(%v, %v, %v): error parsing jwt: %v", userID, tokenSecret, expiresAt, err)
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok || !token.Valid {
+		t.Fatalf("MakeJWT(%v, %v, %v): token not valid", userID, tokenSecret, expiresAt)
+	}
+
+	if claims.Issuer != "chirpy" {
+		t.Fatalf("MakeJWT(%v, %v, %v): expected issuer to be 'chirpy', got %s", userID, tokenSecret, expiresAt, claims.Issuer)
+	}
+	if claims.Subject != userID.String() {
+		t.Fatalf("MakeJWT(%v, %v, %v): expected subject to be '%s', got %s", userID, tokenSecret, expiresAt, userID.String(), claims.Issuer)
+	}
+	if claims.ExpiresAt.Time.Sub(time.Now()) > expiresAt {
+		t.Fatalf("MakeJWT(%v, %v, %v): expected expires_at to be '%v', got %v", userID, tokenSecret, expiresAt, expiresAt, claims.ExpiresAt.Time.Sub(time.Now()))
+	}
+	if claims.IssuedAt.Time.After(time.Now()) {
+		t.Fatalf("MakeJWT(%v, %v, %v): expected issued_at to be '%v', got %v", userID, tokenSecret, expiresAt, time.Now(), claims.IssuedAt.Time.Sub(time.Now()))
 	}
 }
--- internal/auth/main.go
+++ internal/auth/main.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -13,4 +19,23 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	now := time.Now().UTC()
+	expirationTime := now.Add(expiresIn)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  &jwt.NumericDate{Time: now},
+		ExpiresAt: &jwt.NumericDate{Time: expirationTime},
+		Subject:   userID.String(),
+	})
+
+	signed, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signed, nil
 }
